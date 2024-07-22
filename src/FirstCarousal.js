c2ca1da944f67bfb92e17a6c38f4b1d51126bedc
Yash <yashnigam1801@gmail.com>
2024-07-22 13:56:06 +0530

    more changes

--- src/FirstCarousal.js
+++ src/FirstCarousal.js
@@ -3,36 +3,41 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import carous
 import { Carousel } from 'react-responsive-carousel';
 import './FirstCarousal.css'; // Import custom styles
 import { FaArrowRightLong } from "react-icons/fa6";
+
 const FirstCarousal = () => {
   return (
-    <div className="carousel-container">
-      <Carousel
-        showArrows={true}
-        autoPlay={true}
-        infiniteLoop={true}
-        showThumbs={false}
-        showStatus={false}
-        centerMode={true}
-        centerSlidePercentage={50} // Show two items at once
-        dynamicHeight={true} // Adjust height based on current slide
-        emulateTouch={true} // Allow touch swiping
-      >
-        <div className="carousel-item">
-          <img src="https://images.unsplash.com/photo-1721086130975-83605296fdbb?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Slide 1" />
-        </div>
-        <div className="carousel-item">
-          <img src="https://plus.unsplash.com/premium_photo-1719930221771-f04ba1bcfb28?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Slide 2" />
-        </div>
-        <div className="carousel-item">
-          <img src="https://plus.unsplash.com/premium_photo-1720967009356-2689587a0e61?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Slide 3" />
-        </div>
-      </Carousel>
-      <div className="button-container">
-        <button className="btn1 ">Buy Furniture <FaArrowRightLong /></button>
-        <button className="btn2 ">Rent Furniture <FaArrowRightLong /></button>
-        <button className="btn3 ">Unlmtd <FaArrowRightLong /></button>
+    <>
+      <div className="carousel-container">
+        <Carousel
+          showArrows={true}
+          autoPlay={true}
+          infiniteLoop={true}
+          showThumbs={false}
+          showStatus={false}
+          centerMode={true}
+          centerSlidePercentage={50} // Show two items at once
+          dynamicHeight={true} // Adjust height based on current slide
+          emulateTouch={true} // Allow touch swiping
+          swipeable={true} // Allow swipe on touch devices
+          transitionTime={500} // Transition time in milliseconds
+        >
+          <div className="carousel-item">
+            <img src="https://images.unsplash.com/photo-1569458582195-286d4ab6e63e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Slide 1" />
+          </div>
+          <div className="carousel-item">
+            <img src="https://images.unsplash.com/photo-1569458582195-286d4ab6e63e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Slide 2" />
+          </div>
+          <div className="carousel-item">
+            <img src="https://images.unsplash.com/photo-1569458582195-286d4ab6e63e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Slide 3" />
+          </div>
+        </Carousel>
       </div>
-    </div>
+      <div className="button-container">
+        <button className="btn btn1">Buy Furniture <FaArrowRightLong /></button>
+        <button className="btn btn2">Rent Furniture <FaArrowRightLong /></button>
+        <button className="btn btn3">Unlmtd <FaArrowRightLong /></button>
+      </div>
+    </>
   );
 };
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -9,46 +9,58 @@ import { IoIosCheckmarkCircle } from "react-icons/io";
 import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa";
 import { FaShoppingCart} from "react-icons/fa";
+import './Navbar.css'
+
 function NavScrollExample() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="custom-navbar">
       <Container fluid>
-        <Navbar.Brand href="#"> <img src='https://assets.furlenco.com/image/upload/dpr_1.0,f_auto,q_auto/v1/s3-furlenco-images/yoda/furlenco-logo.png' width="190px" alt='logo' /> </Navbar.Brand>
+        <Navbar.Brand href="#">
+          <img 
+            src='https://assets.furlenco.com/image/upload/dpr_1.0,f_auto,q_auto/v1/s3-furlenco-images/yoda/furlenco-logo.png' 
+            width="190px" 
+            alt='logo' 
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
-            className="me-auto my-2 my-lg-0"
+            className="me-auto my-2 my-lg-0 nav-links"
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="#action1"><FaMapLocationDot />Delivery to 560068  </Nav.Link>
-            <Nav.Link href="#action2"> <IoIosCheckmarkCircle />Link</Nav.Link>
-            <NavDropdown title="Buy" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3"> Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-              </NavDropdown.Item>
+            <Nav.Link href="#action1" className="nav-item"><FaMapLocationDot /> Delivery to 560068</Nav.Link>
+            <Nav.Link href="#action2" className="nav-item"><IoIosCheckmarkCircle /> Link</Nav.Link>
+            <NavDropdown title="Buy" id="navbarScrollingDropdown" className="nav-dropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Rent" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3"> Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-              </NavDropdown.Item>
+            <NavDropdown title="Rent" id="navbarScrollingDropdown" className="nav-dropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#" disabled>
-              <img src='https://www.adgully.com/img/800/202107/ulmtd.png.jpg' width="50px" alt='second-img' />
+            <Nav.Link href="#" disabled className="nav-item">
+              <img 
+                src='https://www.adgully.com/img/800/202107/ulmtd.png.jpg' 
+                width="50px" 
+                alt='second-img' 
+              />
             </Nav.Link>
           </Nav>
-          <Form className="d-flex">
+          <Form className="d-flex search-form">
             <Form.Control
               type="search"
               placeholder="What are you searching for"
-              className="me-2"
+              className="me-2 search-input"
               aria-label="Search"
             />
-            <Button variant="outline-success">Search</Button>
+            <Button variant="outline-success" className="search-button">Search</Button>
           </Form>
-          <Nav.Link href="#action2"><FaUser /></Nav.Link>
-          <Nav.Link href="#action2"><FaHeart /></Nav.Link>
-          <Nav.Link href="#action2"><FaShoppingCart /></Nav.Link>
+          <Nav className="ms-3">
+            <Nav.Link href="#action2" className="nav-icon"><FaUser /></Nav.Link>
+            <Nav.Link href="#action2" className="nav-icon"><FaHeart /></Nav.Link>
+            <Nav.Link href="#action2" className="nav-icon"><FaShoppingCart /></Nav.Link>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
